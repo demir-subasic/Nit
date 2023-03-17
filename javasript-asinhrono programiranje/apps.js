6836224ef2bbc7404d647bce31f09b63bbc9fec1
demirsubasic2001 <subasicd2000@gmail.com>
2023-03-17 19:11:42 +0100

    promise

--- javasript-asinhrono programiranje/apps.js
+++ javasript-asinhrono programiranje/apps.js
@@ -1,3 +1,52 @@
-fetch("http://example.com/movies.json")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+// fetch("http://example.com/movies.json")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+
+
+//PROMISE
+
+// let promise = new Promise(function (resolve, reject) {
+//   resolve("Pesma izdata");
+//   reject("Greska se desila");
+// });
+
+// {
+//   state: "pending",
+//   result: undefined,
+// }
+
+// {
+//   state: "fullFiled",
+//   result: value
+// }
+
+// {
+//   state: "rejected",
+//   result: error
+// }
+
+
+
+
+
+
+
+
+
+// let promise = new Promise(function (resolve, reject) {
+
+
+//   setTimeout(() => resolve("done"), 1000)
+// });
+
+
+// console.log(promise);
+
+
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(() => resolve("3 sec after"), ms,));
+}
+
+delay(3000).then((res) => alert(res));
